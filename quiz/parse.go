fc5a72acc2fb2d3c5a415fbac89d126794e46c55
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-17 17:08:18 -0500

    Check for spaces and case-sensitive data

--- quiz/parse.go
+++ quiz/parse.go
@@ -1,11 +1,13 @@
 package main
 
+import "strings"
+
 func parseRecords(records [][]string) []problem {
 	ret := make([]problem, len(records))
 	for i, r := range records {
 		ret[i] = problem{
 			q: r[0],
-			a: r[1],
+			a: strings.ToLower(strings.TrimSpace(r[1])),
 		}
 	}
 	return ret
