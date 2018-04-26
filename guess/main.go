11f19ff1ef9f0c268b3e4b5e7cb2049706e2bc2d
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-26 18:02:52 -0500

    Basic linear number guessing logic

--- guess/main.go
+++ guess/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Executing guess the number")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Guess a number between 1 and 100")
+	fmt.Println("Press ENTER when ready!")
+	scanner.Scan()
+
+	guess := 50
+	for {
+		fmt.Println("I guess the number is: ", guess)
+		fmt.Println("Is that => ")
+		fmt.Println("(a) Too high!")
+		fmt.Println("(b) Too low!")
+		fmt.Println("(c) Correct!")
+		scanner.Scan()
+
+		response := scanner.Text()
+		if response == "a" {
+			guess--
+		} else if response == "b" {
+			guess++
+		} else if response == "c" {
+			fmt.Println("I win!")
+			break
+		} else {
+			fmt.Println("This is not a valid input. Please try again!")
+		}
+	}
 }
