f855e087f6aa8dc3ea69dd6d275e33dcdef47172
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-27 21:51:45 -0500

    Added interactive element to the game

--- linear-story/main.go
+++ linear-story/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type storyPage struct {
 	text     string
@@ -8,18 +12,20 @@ type storyPage struct {
 }
 
 func playStory(page *storyPage) {
+	scanner := bufio.NewScanner(os.Stdin)
+
 	if page == nil {
-		fmt.Println()
 		fmt.Println("End of Story!")
 		return
 	}
 	fmt.Println(page.text)
+	scanner.Scan()
 	playStory(page.nextPage)
 }
 
 func main() {
 	page1 := storyPage{text: "It is a dark and stormy night!", nextPage: nil}
-	page2 := storyPage{text: "You move ahead and try to find the magic helmet before the bad guys fond it!", nextPage: nil}
+	page2 := storyPage{text: "You move ahead and try to find the magic helmet before the bad guys find it!", nextPage: nil}
 	page3 := storyPage{text: "You see a troll ahead in the dark!", nextPage: nil}
 	page1.nextPage = &page2
 	page2.nextPage = &page3
