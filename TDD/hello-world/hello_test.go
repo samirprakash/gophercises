eb0d2ba438b8431b561cd95bbdf1e0cd91cf8239
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-29 00:14:36 -0500

    Refactored tests to add test helper

--- TDD/hello-world/hello_test.go
+++ TDD/hello-world/hello_test.go
@@ -4,22 +4,23 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := hello("Sam")
-		want := "Hello, Sam"
-
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got '%s' want '%s' ", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := hello("Sam")
+		want := "Hello, Sam"
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Saying hello world when an empty string is supplied", func(t *testing.T) {
 		got := hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 }
