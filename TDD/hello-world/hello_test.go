a8a43190e8bd1345d7a6bbcdd46cc1122028423b
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-29 00:52:18 -0500

    Extracted tests to specific files

--- TDD/hello-world/hello_test.go
+++ TDD/hello-world/hello_test.go
@@ -35,30 +35,3 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 }
-
-func TestSalutation(t *testing.T) {
-	assertCorrectMessage := func(t *testing.T, got, want string) {
-		t.Helper()
-		if got != want {
-			t.Errorf("got '%s' want '%s' ", got, want)
-		}
-	}
-
-	t.Run("Salutation should be Hola in spanish", func(t *testing.T) {
-		got := salutation("spanish")
-		want := "Hola, "
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("Salutation should be Bonjour in french", func(t *testing.T) {
-		got := salutation("french")
-		want := "Bonjour, "
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("Salutation should be Hello if no language is specified", func(t *testing.T) {
-		got := salutation("")
-		want := "Hello, "
-		assertCorrectMessage(t, got, want)
-	})
-}
---
+++ TDD/hello-world/salutation_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func TestSalutation(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s' ", got, want)
+		}
+	}
+
+	t.Run("Salutation should be Hola in spanish", func(t *testing.T) {
+		got := salutation("spanish")
+		want := "Hola, "
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Salutation should be Bonjour in french", func(t *testing.T) {
+		got := salutation("french")
+		want := "Bonjour, "
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Salutation should be Hello if no language is specified", func(t *testing.T) {
+		got := salutation("")
+		want := "Hello, "
+		assertCorrectMessage(t, got, want)
+	})
+}
