1a5dd3edd0ab086fa598282d4ea53d2867a733fc
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-28 23:38:50 -0500

    Initial commit for a branching story tree based game

---
+++ branching-story/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type storyNode struct {
+	text    string
+	yesPath *storyNode
+	noPath  *storyNode
+}
+
+func (s *storyNode) play() {
+	fmt.Println(s.text)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if s.yesPath != nil && s.noPath != nil {
+		for {
+			fmt.Print("Choice (yes/no) => ")
+			scanner.Scan()
+			c := scanner.Text()
+
+			if c == "yes" {
+				s.yesPath.play()
+				break
+			} else if c == "no" {
+				s.noPath.play()
+				break
+			} else {
+				fmt.Println("This is not a valid response. Say 'yes' or 'no'")
+			}
+		}
+	}
+}
+
+func main() {
+
+	r := storyNode{text: "You are at the entrance of a cave. Do you want to go in the cave?", yesPath: nil, noPath: nil}
+	w := storyNode{text: "You have won!", yesPath: nil, noPath: nil}
+	l := storyNode{text: "You have lost!", yesPath: nil, noPath: nil}
+	r.yesPath = &l
+	r.noPath = &w
+	r.play()
+}
