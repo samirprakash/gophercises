dad6b6134e9989f5b44081ef166d876e5f332e4c
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-28 23:59:38 -0500

    Updated test and code to address a name passed as a param

--- TDD/hello-world/hello.go
+++ TDD/hello-world/hello.go
@@ -1,5 +1,5 @@
 package main
 
-func hello() string {
-	return "Hello, World"
+func hello(name string) string {
+	return "Hello, " + name
 }
--- TDD/hello-world/hello_test.go
+++ TDD/hello-world/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := hello()
-	want := "Hello, World"
+	got := hello("Sam")
+	want := "Hello, Sam"
 
 	if got != want {
 		t.Errorf("got '%s' want '%s' ", got, want)
