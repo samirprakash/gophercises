6fbf7739d246539a00d5d288a1d17619c5979f98
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-29 17:49:35 -0500

    Example added for documentation

--- TDD/iteration/repeat_test.go
+++ TDD/iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	got := Repeat("a", 7)
@@ -15,4 +18,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 7)
 	}
+}
+
+func ExampleRepeat() {
+	o := Repeat("a", 7)
+	fmt.Println(o)
+	// Output: aaaaaaa
 }
