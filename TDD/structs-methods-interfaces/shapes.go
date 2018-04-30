3b4a06bdc319b70e541890fc32e432aae208e852
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-30 16:48:05 -0500

    Refactored to use structs

--- TDD/structs-methods-interfaces/shapes.go
+++ TDD/structs-methods-interfaces/shapes.go
@@ -1,11 +1,17 @@
 package main
 
-func perimeter(l, b float64) (p float64) {
-	p = 2 * (l + b)
+// Rectangle defines the dimensions
+type Rectangle struct {
+	Length  float64
+	Breadth float64
+}
+
+func perimeter(rectangle Rectangle) (p float64) {
+	p = 2 * (rectangle.Length + rectangle.Breadth)
 	return
 }
 
-func area(l, b float64) (a float64) {
-	a = l * b
+func area(rectangle Rectangle) (a float64) {
+	a = rectangle.Length * rectangle.Breadth
 	return
 }
--- TDD/structs-methods-interfaces/shapes_test.go
+++ TDD/structs-methods-interfaces/shapes_test.go
@@ -3,7 +3,8 @@ package main
 import "testing"
 
 func TestShapes(t *testing.T) {
-	got := perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,7 +13,8 @@ func TestShapes(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := area(12.0, 6.0)
+	rectangle := Rectangle{12.0, 6.0}
+	got := area(rectangle)
 	want := 72.0
 
 	if got != want {
