9e9d9f0818902acdb2d65a9eaebfebcc4d1043af
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-30 17:20:26 -0500

    Implemented table driver tests

--- TDD/structs-methods-interfaces/shapes_test.go
+++ TDD/structs-methods-interfaces/shapes_test.go
@@ -14,22 +14,37 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.area()
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10.0}, 314.1592653589793},
+	}
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+	for _, tt := range areaTests {
+		got := tt.shape.area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
 		}
 	}
 
-	t.Run("calculate area of a rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
+	// checkArea := func(t *testing.T, shape Shape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.area()
 
-	t.Run("calculate area of a circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	// 	if got != want {
+	// 		t.Errorf("got %.2f want %.2f", got, want)
+	// 	}
+	// }
+
+	// t.Run("calculate area of a rectangle", func(t *testing.T) {
+	// 	rectangle := Rectangle{12.0, 6.0}
+	// 	checkArea(t, rectangle, 72.0)
+	// })
+
+	// t.Run("calculate area of a circle", func(t *testing.T) {
+	// 	circle := Circle{10.0}
+	// 	checkArea(t, circle, 314.1592653589793)
+	// })
 }
