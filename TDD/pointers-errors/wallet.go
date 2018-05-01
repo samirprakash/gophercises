c3fc53e55a0afda8183f8e66b00edb49af9e7791
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-05-01 16:57:59 -0500

    Refactored tests/Added withdraw option

--- TDD/pointers-errors/wallet.go
+++ TDD/pointers-errors/wallet.go
@@ -5,12 +5,17 @@ type Wallet struct {
 	balance Bitcoin
 }
 
-// Deposit adds amount to the wallet
+// Deposit adds bitcoins to wallet
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-// Balance returns the amount in a wallet
+// Balance returns the total bitcoins in the wallet
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+// Withdraw removes bitcoin from wallet
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
--- TDD/pointers-errors/wallet_test.go
+++ TDD/pointers-errors/wallet_test.go
@@ -3,12 +3,28 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	w := Wallet{}
-	w.Deposit(Bitcoin(10))
 
-	got := w.Balance()
-	want := Bitcoin(10)
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+	assertBalance := func(t *testing.T, got, want Bitcoin) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("add bitcoin to wallet", func(t *testing.T) {
+		w := Wallet{}
+		w.Deposit(Bitcoin(10))
+
+		got := w.Balance()
+		assertBalance(t, got, Bitcoin(10))
+	})
+
+	t.Run("withdraw bitcoin from wallet", func(t *testing.T) {
+		w := Wallet{balance: Bitcoin(10)}
+		w.Withdraw(Bitcoin(5))
+
+		got := w.Balance()
+		assertBalance(t, got, Bitcoin(5))
+	})
+
 }
