aea786118adfb56639f7306209eb45feb78bfa58
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-05-09 16:55:04 -0500

    Extended use of io.Writer interfce for handling HTTP req/res

--- TDD/dependency-injection/di.go
+++ TDD/dependency-injection/di.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"io"
+	"net/http"
 )
 
 func greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	greet(w, "World")
 }
--- TDD/dependency-injection/main.go
+++ TDD/dependency-injection/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 	fmt.Println("Testing Dependency Injection!")
 	greet(os.Stdout, "Samir")
+
+	http.ListenAndServe(":5000", http.HandlerFunc(greetHandler))
 }
