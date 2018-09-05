447792b84b89583f1f816f680ab43286b4f3b0ee
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-09-05 15:24:37 -0500

    Implemented mapHandler func

--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -12,28 +12,28 @@ func main() {
 	mux := defaultMux()
 
 	pathsToUrls := map[string]string{
-		"/url-shortner-godoc": "https://godoc.org/github.com/samirprakash/urlshort",
-		"yaml-godoc":          "https://godoc.org/gopkg.in/yaml.v2",
+		"/urlshort-godoc": "https://godoc.org/github.com/samirprakash/gophercises/urlshort",
+		"yaml-godoc":      "https://godoc.org/gopkg.in/yaml.v2",
 	}
 
 	// Process the list of URL's via the mux that has been defined
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
 	// Sample YAML inpout data for testing the package implementation
-	yaml := `
-	- path: /urlshort
-		url: https://github.com/samirprakash/urlshort
-	- path: /urlshort-final
-		url: https://github.com/samirprakash/urlshort/tree/solution
-	`
+	// yaml := `
+	// - path: /urlshort
+	// 	url: https://github.com/samirprakash/gophercises/urlshort
+	// - path: /urlshort-final
+	// 	url: https://github.com/samirprakash/urlshort/gophercises/tree/solution
+	// `
 
-	// Process the YAML input using MApHandler
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
+	// // Process the YAML input using MApHandler
+	// yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	// if err != nil {
+	// 	panic(err)
+	// }
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 // Default function to handle fallback scenario when no URL has been provided
@@ -45,5 +45,5 @@ func defaultMux() *http.ServeMux {
 
 // Hanlde `/` base path from the URL
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "Hello World")
+	fmt.Fprintln(w, "Hello World")
 }
